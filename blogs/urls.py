89a6aaad561ab1f4540c4f6f81e427aa6f8c9ff1
Abhimanyu9539 <iabhimanyu31@gmail.com>
2023-08-26 22:39:46 +0530

    Used list views to view a particular post

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),  ## Home page route
+    path('', PostListView.as_view(), name="blog-home"),  ## Home page route
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('about/', views.about, name="blog-about"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post
 
-posts= [
-    {
-        "author":'ABD',
-        "title": "Blog Post 1",
-        "Content": "First Post Content",
-        "date_Posted":"August 21, 2023"
-    },
-        {
-        "author":'ABD',
-        "title": "Blog Post 2",
-        "Content": "Second Post Content",
-        "date_Posted":"August 22, 2023"
-    }
-    
-]
+
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
 
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blogs/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogs/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_Posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, 'blogs/about.html', {'title': 'About'})
+
+
